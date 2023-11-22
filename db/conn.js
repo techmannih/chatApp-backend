59383f72c746a3d3f8ac17b94be77fcc4d91fcc9
techmannih <manishchaudharyttt@gmail.com>
2023-11-23 01:17:50 +0530

    db connected

---
+++ db/conn.js
@@ -0,0 +1,20 @@
+const mongoose =require("mongoose")
+const mongodb=require("mongodb")
+
+const connectDB =async () =>{
+
+    try{
+        const conn= await mongoose.connect(process.env.dbURL, {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useUnifiedTopology: true
+        });
+        console.log("database connected");
+    
+
+    } catch(err){
+        console.log(err.message);
+
+    }
+}
+module.exports = connectDB ;
--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const app = require('express')();
 const server = require('http').createServer(app);
+const dotenv =require("dotenv")
 const io = require('socket.io')(server);
+const connectDB =require("./db/conn")
+
 const port = process.env.PORT || 8080;
 
+connectDB();
+dotenv.config()
 
 app.get('/', function(req, res) {
     res.send("hello from socketio")
