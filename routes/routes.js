82fa5ddc7c30ad9d454e7fa44ba18aee5f2eaf88
techmannih <manishchaudharyttt@gmail.com>
2023-11-23 15:29:33 +0530

    add routes in backend

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,28 @@ const {Router} =require('express')
 
 
 const router =Router();
+// user
+router.route("/users").get(searchAvailableUsers);
+router.route("/addUser").post(addUser);
+router.route("/deleteUser").post(deleteUser);
 
-// router.post('/chat',)
+//chats
+
+router.route("/").get(accessChats);
+
+router.route("/").post(fetchChats);
+router.route("/chats/create").post(createChats);
+
+router.route("/chats/delete").post(deleteChats);
+
+router.route("/chats/new/message").put(addNewMessage);
+
+router.route("/chats/get/message").post(getMessage);
+
+router.route("/chats/delete/message").post(deleteMessage);
+
+router.route("/chats/change/dp").put(chnageDP);
+
+
 
 module.exports= router
