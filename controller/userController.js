c94d37f4dbd8efa240e4b39da8b154d5fea08b57
techmannih <manishchaudharyttt@gmail.com>
2023-12-07 22:12:13 +0530

    correct minor in usercontroller

--- controller/userController.js
+++ controller/userController.js
@@ -16,12 +16,11 @@ module.exports.createUser = async (req, res) => {
     res.status(500).send({ status: false, error: "An error occurred while creating the user." });
   }
 };
+// delete the user account 
 module.exports.deleteUser = async (req, res) => {
     try {
         const userId = req.params.userId;
-
-        // Use Mongoose to find and delete the user
-        const deletedUser = await UserModel.findByIdAndDelete(userId);
+        const deletedUser = await UserModel.findByIdAndDelete(userId); // Use Mongoose to find and delete the user
         if (deletedUser) {
             res.status(200).send({ status: true, message: 'User deleted successfully' });
         } else {
@@ -32,58 +31,57 @@ module.exports.deleteUser = async (req, res) => {
         res.status(500).send({ status: false, message: 'Internal Server Error' });
     }
 };
-
 // find user in db by the email id
 module.exports.findUser= async (req, res) => {
     const email = req.params.email;
     try {
       const user = await UserModel.findOne({ email: email });
       if (user) {
-        res.status(200).send({ status: user });
+        res.status(200).send({ status:true, data:user });
       } else {
-        res.status(200).send({ status: "not found" });
+        res.status(404).send({ status: "not found" });
       }
     } catch (err) {
-      res.status(400).send({ error: "cannot get userdata" });
+      console.log(err);
+      res.status(500).send({ error: "cannot get userdata" });
     }
   };
 //   find user by firebaseid
 module.exports.findUserbyFirebase= async (req, res) => {
     const firebaseUserId = req.params.firebaseUserId;
     try {
-      const user = await UserModel.findOne({ firebaseUserId: firebaseUserId });
+      const user = await UserModel.findOne({ userId: firebaseUserId });
       res.status(200).send({ data: user });
     } catch (err) {
-      res.status(400).send({ error: "not found" });
+      res.status(500).send({ error: "not found" });
     }
   };
 
-//   update user dp
+  // update user dp
 
-module.exports.setProfilePic = async (req, res) => {
-    try {
-        const userId = req.params.userId;
-        const profilePicUrl = req.body.profilePicUrl;
+// module.exports.setProfilePic = async (req, res) => {
+//     try {
+//         const userId = req.params.userId;
+//         const profilePicUrl = req.body.profilePicUrl;
 
-        const updatedUser = await UserModel.findByIdAndUpdate(
-            userId,
-            { $set: { profilePicUrl: profilePicUrl } },
-            { new: true } // Return the updated user
-        );
+//         const updatedUser = await UserModel.findByIdAndUpdate(
+//             userId,
+//             { $set: { profilePicUrl: profilePicUrl } },
+//             { new: true } // Return the updated user
+//         );
 
-        // Check if the user was found and updated successfully
-        if (updatedUser) {
-            res.status(200).send({ status: true, message: 'Profile picture set successfully', user: updatedUser });
-        } else {
-            res.status(404).send({ status: false, message: 'User not found' });
-        }
-    } catch (err) {
-        console.error(err);
-        res.status(500).send({ status: false, message: 'Internal Server Error' });
-    }
-};
+//         // Check if the user was found and updated successfully
+//         if (updatedUser) {
+//             res.status(200).send({ status: true, message: 'Profile picture set successfully', user: updatedUser });
+//         } else {
+//             res.status(404).send({ status: false, message: 'User not found' });
+//         }
+//     } catch (err) {
+//         console.error(err);
+//         res.status(500).send({ status: false, message: 'Internal Server Error' });
+//     }
+// };
 
-// Multer configuration for handling file uploads
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 module.exports.setProfilePic = async (req, res) => {
