f3ba0dabec4fc8cc07871461a191f203ad0409aa
techmannih <manishchaudharyttt@gmail.com>
2023-12-08 14:52:47 +0530

    update chatRoutes

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -6,7 +6,7 @@ const router =Router();
 
 router.route("/chats").get(getAllChats);
 router.route("/chats/create").post(createChats);
-router.route("/chats/delete").delete(deleteChats);
+router.route("/chats/delete/:chatId").delete(deleteChats);
 router.route("/chats/message/new").put(addNewMessage);
 router.route("/chats/message/delete").delete(deleteMessage);
 router.route("/chats/message/update").put(editMessage);
