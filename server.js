0de2f962b709622cac278130d6245b3b020243bf
techmannih <manishchaudharyttt@gmail.com>
2023-12-08 14:54:10 +0530

    üse for json

--- server.js
+++ server.js
@@ -1,23 +1,26 @@
-const app = require('express')();
-const chatRoutes=require("./routes/chatRoutes")
-const userRoutes=require("./routes/userRoutes")
-const server = require('http').createServer(app);
-require("dotenv").config()
+const express = require('express');
+const app = express();
+const chatRoutes = require('./routes/chatRoutes');
+const userRoutes = require('./routes/userRoutes');
+const http = require('http');
+const server = http.createServer(app);
 const io = require('socket.io')(server);
-const connectDB =require("./db/conn")
+const connectDB = require('./db/conn');
+require('dotenv').config();
 
 const port = process.env.PORT || 8888;
 
 connectDB();
 
-app.get('/', function(req, res) {
-    res.send("hello from socketio")
-//    res.sendfile('index.html');
+app.get('/', function (req, res) {
+  res.send('Hello from socket.io');
+  // res.sendfile('index.html');
 });
 
+app.use(express.json());
 app.use(chatRoutes);
 app.use(userRoutes);
 
-server.listen(port, function() {
+server.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
